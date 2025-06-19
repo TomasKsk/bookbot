7dadc70e01a33aeb41824ee0307cf7ec96277beb
TomasKsk <krze.tomas@gmail.com>
2025-06-19 12:29:05 +0200

    Adding sys arguments to take user input

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from stats import (
-    get_num_words,
-    sort_num_dict,
-    get_chars_dict,
+    get_num_words, 
+    get_chars_dict, 
+    sort_num_dict
 )
 
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = sort_num_dict(chars_dict)
-    print_report(book_path, num_words, chars_sorted_list)
+    arguments = sys.argv
+    if len(arguments) == 2:
+        book_path = arguments[1]
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        all_chars = get_chars_dict(text)
+        sort_chars = sort_num_dict(all_chars)
+        print_report(book_path, num_words, sort_chars)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 
-def get_book_text(path):
-    with open(path) as f:
+def get_book_text(file_path):
+    with open(file_path) as f:
         return f.read()
-
-
+    
 def print_report(book_path, num_words, chars_sorted_list):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
@@ -26,11 +31,10 @@ def print_report(book_path, num_words, chars_sorted_list):
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     for item in chars_sorted_list:
-        if not item["char"].isalpha():
-            continue
-        print(f"{item['char']}: {item['num']}")
-
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
     print("============= END ===============")
 
-
 main()
+
+
--- stats.py
+++ stats.py
@@ -2,24 +2,24 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-
 def get_chars_dict(text):
     chars = {}
+
     for c in text:
         lowered = c.lower()
         if lowered in chars:
             chars[lowered] += 1
         else:
             chars[lowered] = 1
+
     return chars
 
-
-def sort_on(d):
-    return d["num"]
-
+def sort_on(dict):
+    return dict["num"]
 
 def sort_num_dict(dict):
     items = dict.items()
     # [k for k in items] - python comprehension to create lists in one line
     key_value_pairs = [{"char": key, "num": value} for key, value in items]
     return sorted(key_value_pairs, reverse=True, key=sort_on)
+
