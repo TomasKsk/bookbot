17c5d7dc2aaaecbb747f2abbf6ebdcca7577b35d
TomasKsk <krze.tomas@gmail.com>
2025-06-19 12:27:02 +0200

    Function to convert and sort a dictionary, Function for printing a structured report

--- stats.py
+++ stats.py
@@ -12,3 +12,14 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def sort_num_dict(dict):
+    items = dict.items()
+    # [k for k in items] - python comprehension to create lists in one line
+    key_value_pairs = [{"char": key, "num": value} for key, value in items]
+    return sorted(key_value_pairs, reverse=True, key=sort_on)
